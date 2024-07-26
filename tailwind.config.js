ade21642085bbfd083c7eb20b2ba5d9777a0ca39
hitiuees <ghbadr132800@gmail.com>
2024-07-26 12:24:50 +0100

    set up

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./pages/**/*.html', // Adjust the path to match where your HTML files are located
+    './pages/main.js',],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
